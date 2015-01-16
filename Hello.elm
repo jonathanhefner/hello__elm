1affd593e0db6b94065d262968157033726a36bf
Jonathan Hefner <jonathan.hefner@gmail.com>
2015-01-16 15:24:13 -0600

    Implement next-on-click functionality

--- Hello.elm
+++ Hello.elm
@@ -1,8 +1,11 @@
 import List
 import Signal
 import Time
+import Time (Time)
 import Html (..)
+import Html.Events (..)
 import Cycler
+import Cycler (Cycler)
 
 
 main : Signal Html
@@ -15,24 +18,42 @@ hellos =
   , "Bonjour tout le Monde"
   , "Hallo Welt"
   ]
+  
+cycleDelay = 2500
+cycleFps = 10 / (cycleDelay / 1000)
 
-type Update = NoOp | Next
+type Update = NoOp | Tick Time | Next
 
-type alias State = Cycler.Cycler String
+type alias State = 
+  { time : Time
+  , cycler : Cycler String
+  }
 
 step : Update -> State -> State
 step u s =
-  case u of
-    NoOp -> s
-    Next -> Cycler.next s
+  let next s' = { initState | cycler <- Cycler.next s'.cycler }
+      nextIf s' = if s'.time >= cycleDelay then (next s') else s'
+  in 
+      case u of
+        NoOp -> s
+        Tick dt -> nextIf { s | time <- (s.time + dt) }
+        Next -> next s
 
 view : State -> Html
-view s = text (Cycler.value s)
+view s = 
+  h1 [ onClick (Signal.send updateChannel Next) ]
+    [ text (Cycler.value s.cycler) ]
 
-initState = Cycler.new hellos
+initState = { time = 0, cycler = Cycler.new hellos }
+
+updateChannel : Signal.Channel Update
+updateChannel = Signal.channel NoOp
 
 updates : Signal Update
-updates = Signal.map (\_ -> Next) (Time.every 1500)
+updates = 
+  Signal.merge
+    (Signal.map (\dt -> Tick dt) (Time.fps cycleFps))
+    (Signal.subscribe updateChannel)
 
 state : Signal State
 state = Signal.foldp step initState updates
